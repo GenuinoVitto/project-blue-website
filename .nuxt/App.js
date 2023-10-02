03d52aaedbe86f93ab5c1643e987434fd8c644ac
Jose Mari Victorio G. Genuino <vitto.g.genuino@gmail.com>
2023-10-02 11:22:53 +0800

    consolidate commit

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,6 +4,7 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from './components/nuxt-error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
+import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\assets\\vendors\\animate\\animate.min.css'
 
@@ -56,7 +57,7 @@ export default {
       }
     }, [
       loadingEl,
-
+      h(NuxtBuildIndicator),
       transitionEl
     ])
   },
@@ -107,10 +108,6 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
-    },
-
-    isPreview () {
-      return Boolean(this.$options.previewData)
     },
   },
 
@@ -201,6 +198,10 @@ export default {
     },
 
     setLayout (layout) {
+      if(layout && typeof layout !== 'string') {
+        throw new Error('[nuxt] Avoid using non-string value as layout property.')
+      }
+
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,66 +1,66 @@
-export const AboutFour = () => import('../..\\components\\AboutFour.vue' /* webpackChunkName: "components/about-four" */).then(c => wrapFunctional(c.default || c))
-export const AboutOne = () => import('../..\\components\\AboutOne.vue' /* webpackChunkName: "components/about-one" */).then(c => wrapFunctional(c.default || c))
-export const AboutThree = () => import('../..\\components\\AboutThree.vue' /* webpackChunkName: "components/about-three" */).then(c => wrapFunctional(c.default || c))
-export const AboutTwo = () => import('../..\\components\\AboutTwo.vue' /* webpackChunkName: "components/about-two" */).then(c => wrapFunctional(c.default || c))
-export const BecomeVolunteerForm = () => import('../..\\components\\BecomeVolunteerForm.vue' /* webpackChunkName: "components/become-volunteer-form" */).then(c => wrapFunctional(c.default || c))
-export const CallToActionOne = () => import('../..\\components\\CallToActionOne.vue' /* webpackChunkName: "components/call-to-action-one" */).then(c => wrapFunctional(c.default || c))
-export const CallToActionTwo = () => import('../..\\components\\CallToActionTwo.vue' /* webpackChunkName: "components/call-to-action-two" */).then(c => wrapFunctional(c.default || c))
-export const CausesCard = () => import('../..\\components\\CausesCard.vue' /* webpackChunkName: "components/causes-card" */).then(c => wrapFunctional(c.default || c))
-export const CausesCarousel = () => import('../..\\components\\CausesCarousel.vue' /* webpackChunkName: "components/causes-carousel" */).then(c => wrapFunctional(c.default || c))
-export const CausesCarouselThree = () => import('../..\\components\\CausesCarouselThree.vue' /* webpackChunkName: "components/causes-carousel-three" */).then(c => wrapFunctional(c.default || c))
-export const CausesCarouselTwo = () => import('../..\\components\\CausesCarouselTwo.vue' /* webpackChunkName: "components/causes-carousel-two" */).then(c => wrapFunctional(c.default || c))
-export const CausesGrid = () => import('../..\\components\\CausesGrid.vue' /* webpackChunkName: "components/causes-grid" */).then(c => wrapFunctional(c.default || c))
-export const CausesPost = () => import('../..\\components\\CausesPost.vue' /* webpackChunkName: "components/causes-post" */).then(c => wrapFunctional(c.default || c))
-export const CausesSidebar = () => import('../..\\components\\CausesSidebar.vue' /* webpackChunkName: "components/causes-sidebar" */).then(c => wrapFunctional(c.default || c))
-export const CausesSingle = () => import('../..\\components\\CausesSingle.vue' /* webpackChunkName: "components/causes-single" */).then(c => wrapFunctional(c.default || c))
-export const ContactHome = () => import('../..\\components\\ContactHome.vue' /* webpackChunkName: "components/contact-home" */).then(c => wrapFunctional(c.default || c))
-export const ContactPageForm = () => import('../..\\components\\ContactPageForm.vue' /* webpackChunkName: "components/contact-page-form" */).then(c => wrapFunctional(c.default || c))
-export const DailyUpdates = () => import('../..\\components\\DailyUpdates.vue' /* webpackChunkName: "components/daily-updates" */).then(c => wrapFunctional(c.default || c))
-export const EventCard = () => import('../..\\components\\EventCard.vue' /* webpackChunkName: "components/event-card" */).then(c => wrapFunctional(c.default || c))
-export const EventCarousel = () => import('../..\\components\\EventCarousel.vue' /* webpackChunkName: "components/event-carousel" */).then(c => wrapFunctional(c.default || c))
-export const EventsGrid = () => import('../..\\components\\EventsGrid.vue' /* webpackChunkName: "components/events-grid" */).then(c => wrapFunctional(c.default || c))
-export const EventSingle = () => import('../..\\components\\EventSingle.vue' /* webpackChunkName: "components/event-single" */).then(c => wrapFunctional(c.default || c))
-export const FeatureOne = () => import('../..\\components\\FeatureOne.vue' /* webpackChunkName: "components/feature-one" */).then(c => wrapFunctional(c.default || c))
-export const FeatureThree = () => import('../..\\components\\FeatureThree.vue' /* webpackChunkName: "components/feature-three" */).then(c => wrapFunctional(c.default || c))
-export const FeatureTwo = () => import('../..\\components\\FeatureTwo.vue' /* webpackChunkName: "components/feature-two" */).then(c => wrapFunctional(c.default || c))
-export const Footer = () => import('../..\\components\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
-export const FunfactOne = () => import('../..\\components\\FunfactOne.vue' /* webpackChunkName: "components/funfact-one" */).then(c => wrapFunctional(c.default || c))
-export const GalleryCard = () => import('../..\\components\\GalleryCard.vue' /* webpackChunkName: "components/gallery-card" */).then(c => wrapFunctional(c.default || c))
-export const GalleryCarousel = () => import('../..\\components\\GalleryCarousel.vue' /* webpackChunkName: "components/gallery-carousel" */).then(c => wrapFunctional(c.default || c))
-export const GalleryGrid = () => import('../..\\components\\GalleryGrid.vue' /* webpackChunkName: "components/gallery-grid" */).then(c => wrapFunctional(c.default || c))
-export const GoogleMap = () => import('../..\\components\\GoogleMap.vue' /* webpackChunkName: "components/google-map" */).then(c => wrapFunctional(c.default || c))
-export const Header = () => import('../..\\components\\Header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
-export const HeaderThree = () => import('../..\\components\\HeaderThree.vue' /* webpackChunkName: "components/header-three" */).then(c => wrapFunctional(c.default || c))
-export const HeaderTwo = () => import('../..\\components\\HeaderTwo.vue' /* webpackChunkName: "components/header-two" */).then(c => wrapFunctional(c.default || c))
-export const MainSliderCounter = () => import('../..\\components\\MainSliderCounter.vue' /* webpackChunkName: "components/main-slider-counter" */).then(c => wrapFunctional(c.default || c))
-export const MainSliderOne = () => import('../..\\components\\MainSliderOne.vue' /* webpackChunkName: "components/main-slider-one" */).then(c => wrapFunctional(c.default || c))
-export const MainSliderThree = () => import('../..\\components\\MainSliderThree.vue' /* webpackChunkName: "components/main-slider-three" */).then(c => wrapFunctional(c.default || c))
-export const MainSliderTwo = () => import('../..\\components\\MainSliderTwo.vue' /* webpackChunkName: "components/main-slider-two" */).then(c => wrapFunctional(c.default || c))
-export const MobileDrawer = () => import('../..\\components\\MobileDrawer.vue' /* webpackChunkName: "components/mobile-drawer" */).then(c => wrapFunctional(c.default || c))
-export const NeedHelp = () => import('../..\\components\\NeedHelp.vue' /* webpackChunkName: "components/need-help" */).then(c => wrapFunctional(c.default || c))
-export const NewsCard = () => import('../..\\components\\NewsCard.vue' /* webpackChunkName: "components/news-card" */).then(c => wrapFunctional(c.default || c))
-export const NewsGrid = () => import('../..\\components\\NewsGrid.vue' /* webpackChunkName: "components/news-grid" */).then(c => wrapFunctional(c.default || c))
-export const NewsHome = () => import('../..\\components\\NewsHome.vue' /* webpackChunkName: "components/news-home" */).then(c => wrapFunctional(c.default || c))
-export const NewsHomeThree = () => import('../..\\components\\NewsHomeThree.vue' /* webpackChunkName: "components/news-home-three" */).then(c => wrapFunctional(c.default || c))
-export const NewsHomeTwo = () => import('../..\\components\\NewsHomeTwo.vue' /* webpackChunkName: "components/news-home-two" */).then(c => wrapFunctional(c.default || c))
-export const NewsSidebar = () => import('../..\\components\\NewsSidebar.vue' /* webpackChunkName: "components/news-sidebar" */).then(c => wrapFunctional(c.default || c))
-export const NewsSingle = () => import('../..\\components\\NewsSingle.vue' /* webpackChunkName: "components/news-single" */).then(c => wrapFunctional(c.default || c))
-export const PageHeader = () => import('../..\\components\\PageHeader.vue' /* webpackChunkName: "components/page-header" */).then(c => wrapFunctional(c.default || c))
-export const Post = () => import('../..\\components\\Post.vue' /* webpackChunkName: "components/post" */).then(c => wrapFunctional(c.default || c))
-export const PostAuthor = () => import('../..\\components\\PostAuthor.vue' /* webpackChunkName: "components/post-author" */).then(c => wrapFunctional(c.default || c))
-export const PostComments = () => import('../..\\components\\PostComments.vue' /* webpackChunkName: "components/post-comments" */).then(c => wrapFunctional(c.default || c))
-export const QuestionAnswer = () => import('../..\\components\\QuestionAnswer.vue' /* webpackChunkName: "components/question-answer" */).then(c => wrapFunctional(c.default || c))
-export const SearchPopup = () => import('../..\\components\\SearchPopup.vue' /* webpackChunkName: "components/search-popup" */).then(c => wrapFunctional(c.default || c))
-export const SectionTitle = () => import('../..\\components\\SectionTitle.vue' /* webpackChunkName: "components/section-title" */).then(c => wrapFunctional(c.default || c))
-export const SponsorsOne = () => import('../..\\components\\SponsorsOne.vue' /* webpackChunkName: "components/sponsors-one" */).then(c => wrapFunctional(c.default || c))
-export const TestimonialsCard = () => import('../..\\components\\TestimonialsCard.vue' /* webpackChunkName: "components/testimonials-card" */).then(c => wrapFunctional(c.default || c))
-export const TestimonialsCarouselOne = () => import('../..\\components\\TestimonialsCarouselOne.vue' /* webpackChunkName: "components/testimonials-carousel-one" */).then(c => wrapFunctional(c.default || c))
-export const TestimonialsGridOne = () => import('../..\\components\\TestimonialsGridOne.vue' /* webpackChunkName: "components/testimonials-grid-one" */).then(c => wrapFunctional(c.default || c))
-export const ThreeBoxes = () => import('../..\\components\\ThreeBoxes.vue' /* webpackChunkName: "components/three-boxes" */).then(c => wrapFunctional(c.default || c))
-export const VideoOne = () => import('../..\\components\\VideoOne.vue' /* webpackChunkName: "components/video-one" */).then(c => wrapFunctional(c.default || c))
-export const VolunteerAbout = () => import('../..\\components\\VolunteerAbout.vue' /* webpackChunkName: "components/volunteer-about" */).then(c => wrapFunctional(c.default || c))
-export const VolunteerCard = () => import('../..\\components\\VolunteerCard.vue' /* webpackChunkName: "components/volunteer-card" */).then(c => wrapFunctional(c.default || c))
-export const VolunteerGrid = () => import('../..\\components\\VolunteerGrid.vue' /* webpackChunkName: "components/volunteer-grid" */).then(c => wrapFunctional(c.default || c))
+export { default as AboutFour } from '../..\\components\\AboutFour.vue'
+export { default as AboutOne } from '../..\\components\\AboutOne.vue'
+export { default as AboutThree } from '../..\\components\\AboutThree.vue'
+export { default as AboutTwo } from '../..\\components\\AboutTwo.vue'
+export { default as BecomeVolunteerForm } from '../..\\components\\BecomeVolunteerForm.vue'
+export { default as CallToActionOne } from '../..\\components\\CallToActionOne.vue'
+export { default as CallToActionTwo } from '../..\\components\\CallToActionTwo.vue'
+export { default as CausesCard } from '../..\\components\\CausesCard.vue'
+export { default as CausesCarousel } from '../..\\components\\CausesCarousel.vue'
+export { default as CausesCarouselThree } from '../..\\components\\CausesCarouselThree.vue'
+export { default as CausesCarouselTwo } from '../..\\components\\CausesCarouselTwo.vue'
+export { default as CausesGrid } from '../..\\components\\CausesGrid.vue'
+export { default as CausesPost } from '../..\\components\\CausesPost.vue'
+export { default as CausesSidebar } from '../..\\components\\CausesSidebar.vue'
+export { default as CausesSingle } from '../..\\components\\CausesSingle.vue'
+export { default as ContactHome } from '../..\\components\\ContactHome.vue'
+export { default as ContactPageForm } from '../..\\components\\ContactPageForm.vue'
+export { default as DailyUpdates } from '../..\\components\\DailyUpdates.vue'
+export { default as EventCard } from '../..\\components\\EventCard.vue'
+export { default as EventCarousel } from '../..\\components\\EventCarousel.vue'
+export { default as EventsGrid } from '../..\\components\\EventsGrid.vue'
+export { default as EventSingle } from '../..\\components\\EventSingle.vue'
+export { default as FeatureOne } from '../..\\components\\FeatureOne.vue'
+export { default as FeatureThree } from '../..\\components\\FeatureThree.vue'
+export { default as FeatureTwo } from '../..\\components\\FeatureTwo.vue'
+export { default as Footer } from '../..\\components\\Footer.vue'
+export { default as FunfactOne } from '../..\\components\\FunfactOne.vue'
+export { default as GalleryCard } from '../..\\components\\GalleryCard.vue'
+export { default as GalleryCarousel } from '../..\\components\\GalleryCarousel.vue'
+export { default as GalleryGrid } from '../..\\components\\GalleryGrid.vue'
+export { default as GoogleMap } from '../..\\components\\GoogleMap.vue'
+export { default as Header } from '../..\\components\\Header.vue'
+export { default as HeaderThree } from '../..\\components\\HeaderThree.vue'
+export { default as HeaderTwo } from '../..\\components\\HeaderTwo.vue'
+export { default as MainSliderCounter } from '../..\\components\\MainSliderCounter.vue'
+export { default as MainSliderOne } from '../..\\components\\MainSliderOne.vue'
+export { default as MainSliderThree } from '../..\\components\\MainSliderThree.vue'
+export { default as MainSliderTwo } from '../..\\components\\MainSliderTwo.vue'
+export { default as MobileDrawer } from '../..\\components\\MobileDrawer.vue'
+export { default as NeedHelp } from '../..\\components\\NeedHelp.vue'
+export { default as NewsCard } from '../..\\components\\NewsCard.vue'
+export { default as NewsGrid } from '../..\\components\\NewsGrid.vue'
+export { default as NewsHome } from '../..\\components\\NewsHome.vue'
+export { default as NewsHomeThree } from '../..\\components\\NewsHomeThree.vue'
+export { default as NewsHomeTwo } from '../..\\components\\NewsHomeTwo.vue'
+export { default as NewsSidebar } from '../..\\components\\NewsSidebar.vue'
+export { default as NewsSingle } from '../..\\components\\NewsSingle.vue'
+export { default as PageHeader } from '../..\\components\\PageHeader.vue'
+export { default as Post } from '../..\\components\\Post.vue'
+export { default as PostAuthor } from '../..\\components\\PostAuthor.vue'
+export { default as PostComments } from '../..\\components\\PostComments.vue'
+export { default as QuestionAnswer } from '../..\\components\\QuestionAnswer.vue'
+export { default as SearchPopup } from '../..\\components\\SearchPopup.vue'
+export { default as SectionTitle } from '../..\\components\\SectionTitle.vue'
+export { default as SponsorsOne } from '../..\\components\\SponsorsOne.vue'
+export { default as TestimonialsCard } from '../..\\components\\TestimonialsCard.vue'
+export { default as TestimonialsCarouselOne } from '../..\\components\\TestimonialsCarouselOne.vue'
+export { default as TestimonialsGridOne } from '../..\\components\\TestimonialsGridOne.vue'
+export { default as ThreeBoxes } from '../..\\components\\ThreeBoxes.vue'
+export { default as VideoOne } from '../..\\components\\VideoOne.vue'
+export { default as VolunteerAbout } from '../..\\components\\VolunteerAbout.vue'
+export { default as VolunteerCard } from '../..\\components\\VolunteerCard.vue'
+export { default as VolunteerGrid } from '../..\\components\\VolunteerGrid.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
